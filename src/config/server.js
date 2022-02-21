c83da447a4d4299819255ee2a749e3b47fd09fcd
Carlos Jaldin <carjal192000@gmail.com>
2022-02-21 01:00:46 -0400

    basic configurations

---
+++ src/config/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+var path = require('path');
+class Server {
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.userRoutePath = '/api/users'
+        //Middlewares
+        this.middlewares();
+        //rutas
+        this.routes();
+    }
+
+    middlewares(){
+        //CORS
+        this.app.use(cors());
+        //lectura y parseo del body
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: true}));
+        //directorio publico
+        this.app.use(express.static(path.join(__dirname, '../../public')));
+    }
+
+    routes(){
+        this.app.use(this.userRoutePath, require('../routes/user.routes'));
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en el puerto --> ', this.port);
+        });
+    }
+}
+
+module.exports = Server;
+
